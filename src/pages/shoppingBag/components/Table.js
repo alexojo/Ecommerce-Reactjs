446a9c628d6b8e1cf2f0b913f82607d89900dc17
jorge ANDRE SALCEDO hurtado <171572@unsaac.edu.pe>
2021-03-10 00:12:49 -0500

    Final update favorites

--- src/pages/shoppingBag/components/Table.js
+++ src/pages/shoppingBag/components/Table.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import styles from "../stylesheets/table.module.sass";
 import { Button } from "react-bootstrap";
-import jumpTo from '../../../modules/Navigation'
+import jumpTo from "../../../modules/Navigation";
 
-export default function BagTable({ items, handleClick, props}) {
-  
+export default function BagTable({ items, handleClick, props }) {
   return (
     <div className={styles.outbox}>
       <Table striped bordered hover>
@@ -16,33 +15,67 @@ export default function BagTable({ items, handleClick, props}) {
             <th>Titulo</th>
             <th>Contacto</th>
             <th>Precio</th>
+            <th>Eliminar</th>
           </tr>
         </thead>
         <tbody className={styles.table_body}>
           {Object.keys(items).map((id) => (
             // table row
             <tr key={id}>
-              {/* pic */}
+              {/* imagen */}
               <td>
-              <div className={styles.pic_outbox}
-                  onClick={() => jumpTo(`/product-overview/${items[id].item._id}`)}>
+                <div
+                  className={styles.pic_outbox}
+                  onClick={() =>
+                    jumpTo(`/product-overview/${items[id].item._id}`)
+                  }
+                >
                   <img src={items[id].item.imagePath} alt="" />
                 </div>
               </td>
-              {/* title */}
+              {/* titulo */}
               <td>
-                <div className={styles.title_outbox}>
+                <div
+                  className={styles.title_outbox}
+                  onClick={() =>
+                    jumpTo(`/product-overview/${items[id].item._id}`)
+                  }
+                >
                   <div className={styles.title}>{items[id].item.title}</div>
                   <div className={styles.id_title}>Descripción:</div>
                   <div className={styles.id}>{items[id].item.description}</div>
                 </div>
               </td>
               {/* contacto */}
-              <td>
+              <td
+                onClick={() =>
+                  jumpTo(`/product-overview/${items[id].item._id}`)
+                }
+              >
                 <div className={styles.id}>{items[id].item.cellphone}</div>
               </td>
-              {/* price */}
-              <td>{items[id].price / items[id].qty}</td>
+              {/* precio */}
+              <td
+                onClick={() =>
+                  jumpTo(`/product-overview/${items[id].item._id}`)
+                }
+              >
+                {items[id].price}
+              </td>
+              {/* eliminar */}
+              <td>
+                <div className={styles.qty_outbox}>
+                  <Button
+                    variant="outline-secondary"
+                    className={styles.btn}
+                    onClick={() => {
+                      handleClick(id, false, true);
+                    }}
+                  >
+                    <span>❌</span>
+                  </Button>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
